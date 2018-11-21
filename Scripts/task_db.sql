35caa5c1c05c9590e4d6f8c3de391b797839b51e
Kryshneu Siarhei <skryshneu@exadel.by>
2018-11-21 15:22:13 +0300

    project finished

--- Scripts/task_db.sql
+++ Scripts/task_db.sql
@@ -3,8 +3,8 @@ SET search_path TO skill_schema;
 SELECT s.name_skill, s.year_skill, s.last_used_skill, s.last_check_skill, s.checked_by_skill,
 st.name_skill_type AS name_type, ss.name_skill_status AS name_status  
 FROM skills s
-LEFT OUTER JOIN skill_types st ON s.id_type = st.id
-LEFT OUTER JOIN skill_statuses ss ON s.id_status = ss.id
+INNER JOIN skill_types st ON s.id_type = st.id
+INNER JOIN skill_statuses ss ON s.id_status = ss.id
 WHERE name_skill LIKE 'Industry Vertical %'
 ORDER BY year_skill;
 --2) Update the record by condition
@@ -14,3 +14,28 @@ WHERE name_skill = 'Industry Vertical %';
 --3) Delete the record by condition
 DELETE FROM skills
 WHERE year_skill = 3;
+--4) New Task: Update by skill_type
+
+UPDATE skills
+SET id_type = 103
+WHERE id in (
+	SELECT s.id
+	FROM skills s
+	INNER JOIN skill_types st ON s.id_type = st.id
+	INNER JOIN skill_statuses ss ON s.id_status = ss.id
+	AND ss.name_skill_status = 'Announced');
+	
+--5) New Task: Delete by skill_type and skill_status
+DELETE FROM skills
+WHERE id in (
+	SELECT s.id 
+	FROM skills s
+	INNER JOIN skill_types st ON s.id_type = st.id
+	INNER JOIN skill_statuses ss ON s.id_status = ss.id
+	AND st.name_skill_type = 'Empty type'
+	AND ss.name_skill_status = 'Announced'
+)
+SELECT s.year_skill, st.name_skill_type AS name_type  
+	FROM skills s
+	INNER JOIN skill_types st ON s.id_type = st.id
+	GROUP BY s.year_skill, st.name_skill_type;
