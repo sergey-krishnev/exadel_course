f1333b2ce6c3348fd359167bcaa0df9e19d4caa4
Kryshneu Siarhei <skryshneu@exadel.by>
2018-12-17 15:44:50 +0300

    hello world stage

---
+++ SpringTask/src/main/java/com/spring/SpringTask/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.spring.SpringTask;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SpringTask/src/main/java/com/spring/SpringTask/SpringTaskApplication.java
@@ -0,0 +1,17 @@
+package com.spring.SpringTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringTaskApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SpringTaskApplication.class, args);
+		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+		helloWorld.sayHello();
+	}
+
+}
+
