e68300fab1ecaa693f7c742de6a011c5286eb5f5
Сергей <serega.maestro.97@gmail.com>
2018-12-19 11:38:10 +0300

    xml configuration (checkpoint)

--- SpringTask/src/main/java/com/spring/SpringTask/SpringTaskApplication.java
+++ SpringTask/src/main/java/com/spring/SpringTask/SpringTaskApplication.java
@@ -1,13 +1,17 @@
 package com.spring.SpringTask;
 
+import hibernate.dao.Interfaces.CRUDDao;
+import hibernate.dao.Interfaces.DisplayDao;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringTaskApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SpringTaskApplication.class, args);
-		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-		helloWorld.sayHello();
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-hibernate-xml.xml");
+		CRUDDao crudDao = (CRUDDao) context.getBean("crudDao");
+		DisplayDao displayDao = (DisplayDao) context.getBean("displayDao");
+		displayDao.display(crudDao.searchAll());
 	}
 
 }
