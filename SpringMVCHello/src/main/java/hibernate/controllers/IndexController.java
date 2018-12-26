b3d37b5af45ee27c72643f011f0b29b3ab704ad1
Kryshneu Siarhei <skryshneu@exadel.by>
2018-12-26 16:01:09 +0300

    working simple output but not data

---
+++ SpringMVCHello/src/main/java/hibernate/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package hibernate.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/MainServlet", method = RequestMethod.GET)
+    public String index(Model m) {
+        m.addAttribute("someAttribute", "someValue");
+        return "index";
+    }
+}
