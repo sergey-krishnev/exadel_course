43b91b8a85a22634ab17d1768fb57a77b5fdb8bd
Kryshneu Siarhei <skryshneu@exadel.by>
2018-12-27 15:47:06 +0300

    working home page with delete and add

--- SpringMVCForumTask/src/main/java/hibernate/dao/interfaces/UpdateOperation.java
+++
@@ -1,9 +0,0 @@
-package hibernate.dao.interfaces;
-
-import java.sql.Date;
-
-public interface UpdateOperation {
-
-    void updateSubjectById(Integer id, String username, String topicName, String subjectName,
-                           String message, Date date);
-}
