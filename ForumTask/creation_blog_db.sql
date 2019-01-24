b3e450188c97fe619653ce901d2e3310ef645a84
Kryshneu Siarhei <skryshneu@exadel.by>
2019-01-24 16:18:07 +0300

    created comment in database

---
+++ ForumTask/creation_blog_db.sql
@@ -0,0 +1,43 @@
+CREATE SCHEMA IF NOT EXISTS forum_schema;
+SET search_path TO forum_schema;
+	CREATE TABLE users
+	(
+		id BIGINT PRIMARY KEY,
+		nickname CHARACTER VARYING(30) NOT NULL,
+		password CHARACTER VARYING(50) NOT NULL,
+		email CHARACTER VARYING(40) NOT NULL,
+		first_name CHARACTER VARYING(15) NOT NULL,
+		last_name CHARACTER VARYING(20) NOT NULL
+	);
+	CREATE TABLE topic
+	(
+		id BIGINT PRIMARY KEY,
+		name CHARACTER VARYING(100) NOT NULL
+	);
+	CREATE TABLE subject
+	(
+		id BIGINT PRIMARY KEY,
+		name CHARACTER VARYING(100) NOT NULL,
+		date DATE NOT NULL,
+		user_id BIGINT NOT NULL,
+		topic_id BIGINT NOT NULL
+	);
+	CREATE TABLE comment
+	(
+		id BIGINT PRIMARY KEY,
+		message CHARACTER VARYING(256) NOT NULL,
+		date DATE NOT NULL,
+		subject_id BIGINT NOT NULL,
+		user_id BIGINT NOT NULL
+	);
+	ALTER TABLE forum_schema.subject
+	ADD CONSTRAINT fk_users_subject FOREIGN KEY (user_id) REFERENCES users (id);
+	
+	ALTER TABLE forum_schema.subject
+	ADD CONSTRAINT fk_topic_subject FOREIGN KEY (topic_id) REFERENCES topic (id);
+	
+	ALTER TABLE forum_schema.comment
+	ADD CONSTRAINT fk_subject_message FOREIGN KEY (subject_id) REFERENCES subject (id);
+	
+	ALTER TABLE forum_schema.comment
+	ADD CONSTRAINT fk_users_comment FOREIGN KEY (user_id) REFERENCES users (id);
